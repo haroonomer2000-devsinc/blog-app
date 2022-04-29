ddb69f376e3e51218910f88cef7fe61702674b3e
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-04-29 19:37:13 +0500

    Add email confirmation for user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,18 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      "haroon.omer@devsinc.com",
+    password:       "hfvioefanguszylv",
+    domain:         'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create(title: "Post 1 title",description:"Post 1 desc")
+Post.create(title: "Post 2 title",description:"Post 2 desc")
+Post.create(title: "Post 3 title",description:"Post 3 desc")
+Post.create(title: "Post 4 title",description:"Post 4 desc")
