533af21ed731902a067be50bcddfc976eb9bdd7b
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-05-12 01:33:45 +0500

    Add flash messages and alerts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
     def create
         @comment = current_user.comments.new(comment_params)
         if !@comment.save
-            flash[:notice] = @comment.errors.full_messages.to_sentence
+            flash[:alert] = @comment.errors.full_messages.to_sentence
         end
         redirect_to post_path(params[:post_id])
     end
