ff210edf6f66735b64f9db4e25fd7a14912c45b7
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-05-13 19:20:37 +0500

    Add ability to attach multiple files with comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  has_many_attached :files
   validates :body, presence: :true
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :comments, foreign_key: :parent_id
