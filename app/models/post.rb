1ed8b73e10f1aa2bac122960074ed716d401195b
haroon-omer2000 <haroonomer2000@yahoo.com>
2022-05-16 18:25:30 +0500

    Add ability to like comments and posts through polymorphic association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :suggestions, dependent: :destroy
-  validates :title, presence: :true
-  validates :description, presence: :true
+  has_many :likes, as: :likeable
   has_many_attached :files
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: :true
+  validates :description, presence: :true
+
   enum status: { unpublished: 0, published: 1 }
 
   def apply_suggestion(suggestion)
