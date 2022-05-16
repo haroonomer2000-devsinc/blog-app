72d20bfff4f3c118be68fab74076c473ec4213a7
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-05-16 19:30:56 +0500

    Add EOF, remove custom error pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :suggestions, dependent: :destroy
-  has_many :likes, as: :likeable
+  has_many :likes, as: :likeable, dependent: :destroy
   has_many_attached :files
   has_many :comments, dependent: :destroy
 
   validates :title, presence: :true
   validates :description, presence: :true
 
-  enum status: { unpublished: 0, published: 1 }
+  enum status: { UNPUBLISHED: 0, PUBLISHED: 1 }
 
   def apply_suggestion(suggestion)
     self.description.gsub!(/#{suggestion.to_replace}/i, suggestion.replacement)
