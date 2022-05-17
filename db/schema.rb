23fd0be8f0464e6bdf064fd6c3f13a0bcaa1e6d2
haroon-omer2000 <haroonomer2000@yahoo.com>
2022-05-17 14:38:04 +0500

    Add report comment functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_125827) do
+ActiveRecord::Schema.define(version: 2022_05_17_084240) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_125827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "parent_id"
+    t.string "status"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
