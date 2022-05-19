0dbc8274a98683a15d0bc6c424c1801177bb0b41
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-05-19 21:49:13 +0500

    update prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'shielded-wave-54850.herokuapp.com/' }
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.email_config[:email],
+    password: Rails.application.credentials.email_config[:password],
+    domain: 'shielded-wave-54850.herokuapp.com',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   config.active_storage.service = :cloudinary
 end
