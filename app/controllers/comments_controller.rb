90f54aecb56fe768bf3b4193a1c5c8251d396733
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-05-20 18:13:52 +0500

    change double negation to unless

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.new(comment_params.merge(post_id: params[:post_id]))
-    if !@comment.save
+    unless @comment.save
       flash[:alert] = @comment.errors.full_messages.to_sentence
     end
     redirect_to post_path(params[:post_id])
