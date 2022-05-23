4403380c50262d78545a93f423158c26cc277f76
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-05-23 17:20:02 +0500

    Add email confirmation message after signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/confirmation'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :likes, only: [:create, :destroy]
   resources :suggestions do 
     collection do 
