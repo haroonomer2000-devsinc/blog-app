516210b75b4e6995e7a532c9d89c862f8c4d5723
haroon-omer2000 <haroonomer2000@yahoo.com>
2022-05-24 20:15:46 +0500

    fix branch condition size

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,10 +71,8 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to post_url(@post), notice: I18n.t(:post_update) }
-        format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
