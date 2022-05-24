29347432824edf1b6968d4aa694ef24609084c15
haroon-omer2000 <haroonomer2000@yahoo.com>
2022-05-24 19:33:22 +0500

    specify dependant for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :suggestions, dependent: :destroy
