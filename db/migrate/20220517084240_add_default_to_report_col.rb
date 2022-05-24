aaf4a6e9e714ed0eb6355d3e9128e081d4110bf9
haroon-omer2000 <haroonomer2000@yahoo.com>
2022-05-24 19:23:14 +0500

    change migration to reversible

--- db/migrate/20220517084240_add_default_to_report_col.rb
+++ db/migrate/20220517084240_add_default_to_report_col.rb
@@ -3,6 +3,6 @@
 class AddDefaultToReportCol < ActiveRecord::Migration[5.2]
   def change
     rename_column :comments, :reported, :status
-    change_column :comments, :status, :string, default: nil
   end
+  change_column :comments, :status, :string, default: nil
 end
