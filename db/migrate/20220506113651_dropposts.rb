c6b85dac3e885d1b394243687ea32cc521152cbb
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-05-24 19:23:14 +0500

    change migration to reversible

--- db/migrate/20220506113651_dropposts.rb
+++ db/migrate/20220506113651_dropposts.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Dropposts < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :posts
-  end
+  def change; end
+  drop_table :posts
 end
--- db/migrate/20220514120334_drop_suggestion.rb
+++ db/migrate/20220514120334_drop_suggestion.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class DropSuggestion < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :suggestions
-  end
+  def change; end
+  drop_table :suggestions
 end
