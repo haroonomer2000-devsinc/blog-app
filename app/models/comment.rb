7ac86d677e78b6c4c057664a306e1d5b3b020ac0
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-05-24 19:14:46 +0500

    Add dependent destroy to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: 'Comment', optional: true
 
   has_many_attached :files
-  has_many :comments, foreign_key: :parent_id
+  has_many :comments, dependent: :destroy, foreign_key: :parent_id
   has_many :likes, as: :likeable, dependent: :destroy
 
   validates :body, presence: true
