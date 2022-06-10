cde8d23234c5990f04435451fb7658a22b8e5b14
haroon-omer2000 <haroonomer2000@yahoo.com>
2022-06-10 20:35:57 +0500

    obtained 92% coverage for post controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def confirmation; end
+  def confirmation
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,10 +4,10 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(_resource)
-    home_confirmation_path # Or :prefix_to_your_route
+    confirmation_home_index_path # Or :prefix_to_your_route
   end
 
   def after_inactive_sign_up_path_for(_resource)
-    home_confirmation_path # Or :prefix_to_your_route
+    confirmation_home_index_path # Or :prefix_to_your_route
   end
 end
