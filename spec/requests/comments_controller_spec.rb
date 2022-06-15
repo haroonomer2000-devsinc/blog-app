b217161a53669aa12888741159b67e983ce71f3d
haroonomer2000-devsinc <haroon.omer@devsinc.com>
2022-06-15 17:49:18 +0500

    change spec file

--- spec/requests/comments_controller_spec.rb
+++ spec/requests/comments_controller_spec.rb
@@ -7,16 +7,19 @@ RSpec.describe "Comments Controller", type: :request do
       user = users(:moderator)
       user.confirm
       sign_in user
+      @post_id = 134
   end
 
   let(:valid_post) do
     {
       'id' => '4',
       'title' => "Test Post 4",
-      'description' => "This is the description of post number 4", 'user_id'=> 707834473, 
+      'description' => "This is the description of post number 4", 
+      'user_id'=> 707834473, 
       'status' => 0 
     }
   end
+
   let(:invalid_post) do
     {
       'description' => "This is the description of post number 4", 
@@ -26,7 +29,6 @@ RSpec.describe "Comments Controller", type: :request do
   end
   let (:valid_comment) do 
     {
-        'id' => '1',
         'body' => "This is test comment body",
         'user_id' => 175178709,
         'post_id' => 134,
@@ -35,15 +37,17 @@ RSpec.describe "Comments Controller", type: :request do
   end
   
   describe 'Comments /create' do
-    context "Comments create path with valid attributes" do 
-      it 'creates a new comment' do
-        post_id = 134
-        post = Post.find(post_id)
+    context "Comments create path" do 
+      it 'creates a new comment with invalid attrs' do
+        sign_out :user
+        user = users(:regular)
+        user.confirm
+        sign_in user
+        post = Post.find(@post_id)
         expect do 
-          comment = Comment.new(valid_comment)
-          comment.save
-          post post_comments_url(post.id), params: {comment: valid_comment, post: post.attributes }
-        end.to change(Comment, :count).by(1)
+          @comment = Comment.new(valid_comment)
+          post post_comments_url(post.id), params: {body: "asasd", post: post.attributes }
+        end.to change(Comment, :count).by(0)
       end
     end
   end
@@ -51,24 +55,22 @@ RSpec.describe "Comments Controller", type: :request do
   describe 'Comments /update' do
     context "Comments update path with valid attributes" do 
       it 'updates a comment' do
-        post_id = 134
-        post = Post.find(post_id)
+        post = Post.find(@post_id)
         comment = Comment.new(valid_comment)
         comment.save
-        patch post_comment_url(post.id,comment), params: {comment: valid_comment, post: post.attributes }
+        patch post_comment_url(post.id,comment), params: {comment: valid_comment, post: post.attributes}
       end
     end
   end
 
   describe 'Comments /delete' do
     context "Comments delete path" do 
-      it 'deletes a comment' do
+      it 'deletes a valid comment' do
         sign_out :user
         user = users(:regular)
         user.confirm
         sign_in user
-        post_id = 134
-        post = Post.find(post_id)
+        post = Post.find(@post_id)
         comment = Comment.new(valid_comment)
         comment.save
         expect do
@@ -76,16 +78,15 @@ RSpec.describe "Comments Controller", type: :request do
         end.to change(Comment, :count).by(-1)
       end
 
-      it 'deletes a comment' do
+      it 'deletes a comment with invalid id resource not found' do
         sign_out :user
         user = users(:regular)
         user.confirm
         sign_in user
-        post_id = 134
-        post = Post.find(post_id)
+        post = Post.find(@post_id)
         comment = Comment.new(valid_comment)
         comment.save
-        comment.id = 123 #invalid id
+        comment.id = 123123 #invalid id
         delete post_comment_url(post.id,comment), params: {comment: comment, post: post.attributes }
         expect(response).to have_http_status(302)
       end
